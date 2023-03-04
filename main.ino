a565483d6c5377dc404115c24a772ca524b48507
TheronAB <100641656+TheronAB@users.noreply.github.com>
2023-03-03 16:44:13 -0800

    V 0.2
    Updated program, removed unnecessary documents from testing branch.

--- main.ino
+++ main.ino
@@ -31,33 +31,57 @@ void setup() {
 }
 
 void loop() {
-  voltAadc = analogRead(voltPinA);
-  voltBadc = analogRead(voltPinB);
-  voltCadc = analogRead(voltPinC);
-  currentAadc = analogRead(currentPinA);
-  currentBadc = analogRead(currentPinB);
-  currentCadc = analogRead(currentPinC);
-  //tempadc = analogRead(tempPin);
-  voltValA = floor(0.00080586*voltAadc);
-  voltValB = floor(0.00080586*voltBadc);
-  voltValC = floor(0.00080586*voltCadc);
-  currentValA = floor(0.00080586*currentAadc);
-  currentValB = floor(0.00080586*currentBadc);
-  currentValC = floor(0.00080586*currentCadc);
-  //tempVal = floor(0.00080586*tempadc);
-  delay(1000);
+  i = 0;
+  Va = 0;
+  Vb = 0;
+  Vc = 0;
+  Aa = 0;
+  Ab = 0;
+  Ac = 0;
+  //T = 0;
+  while (i < 100):
+    voltAadc = analogRead(voltPinA);
+    voltBadc = analogRead(voltPinB);
+    voltCadc = analogRead(voltPinC);
+    currentAadc = analogRead(currentPinA);
+    currentBadc = analogRead(currentPinB);
+    currentCadc = analogRead(currentPinC);
+    //tempadc = analogRead(tempPin);
+    voltValA = floor(0.00080586*voltAadc);
+    voltValB = floor(0.00080586*voltBadc);
+    voltValC = floor(0.00080586*voltCadc);
+    currentValA = floor(0.00080586*currentAadc);
+    currentValB = floor(0.00080586*currentBadc);
+    currentValC = floor(0.00080586*currentCadc);
+    //tempVal = floor(0.00080586*tempadc);
+    if (voltValA > Va):
+      Va = voltValA;
+    if (voltValB > Vb):
+      Vb = voltValB;
+    if (voltValC > Vc):
+      Vc = voltValC;
+    if (currentValA > Aa):
+      Aa = currentValA;
+    if (currentValB > Ab):
+      Ab = currentValB;
+    if (currentValC > Ac):
+      Ac = currentValC;
+    //if (tempVal > T):
+    //  T = tempVal  
+    i += 1;    
+  delay(100);
   Serial.print("Phase A: voltage = ");
-  Serial.print(voltValA);
+  Serial.print(Va);
   Serial.print(" current = ");
-  Serial.print(currentValA);
+  Serial.print(Aa);
   Serial.print(" Phase B: voltage = ");
-  Serial.print(voltValB);
+  Serial.print(Vb);
   Serial.print(" current = ");
-  Serial.print(currentValB);
+  Serial.print(Ab);
   Serial.print(" Phase C: voltage = ");
-  Serial.print(voltValC);
+  Serial.print(Vc);
   Serial.print(" current = ");
-  Serial.print(currentValC);
+  Serial.print(Ac);
   //Serial.print("Temperature = ");
   //Serial.print(tempVal);
 }
